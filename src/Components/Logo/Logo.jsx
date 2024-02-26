ed2fb17f70362fcd3d5df199a1215839ae8876f6
DouglassY <teryakiboyzv2@gmail.com>
2024-02-26 12:36:21 +0300

    adding Logo image

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Tilt from "react-parallax-tilt";
+import FaceLogo from "./FaceLogo.png";
 
 const Logo = () => {
   return (
-    <div className="m-4 mt-0 inline-block">
+    <div className="m-4 mt-0 inline-block hover:scale-110 duration-300  absolute top-3">
       <Tilt tiltReverse:false>
-        <div className="w-40 h-40  bg-gradient-to-tr from-cyan-200 to-white flex justify-center items-center">
-          <p className="text-9xl">🗿</p>
+        <div className="w-40 h-40  bg-gradient-to-tr from-cyan-200 to-white flex justify-center items-center rounded-xl relative top-0">
+          <p className="p-2">
+            <img src={FaceLogo} alt="Logo" />
+          </p>
         </div>
       </Tilt>
     </div>
