7b3467abb9224adc963ac05fd68fe2f8ff9a973b
DouglassY <teryakiboyzv2@gmail.com>
2024-03-02 00:30:09 +0300

    Adding clarifai api

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,77 @@ import React, { Component } from "react";
 import Navigation from "./Components/Navigation/Navigation";
 import Logo from "./Components/Logo/Logo";
 import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
-
+import Clarifai from "clarifai";
 import "./App.css";
 
+const app = new Clarifai.App({
+  apiKey: "b6dd9aa133da413ca1b281c11cea2841",
+});
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log("click");
+
+    const PAT = "b6dd9aa133da413ca1b281c11cea2841";
+    const USER_ID = "douglasyy1059";
+    const APP_ID = "Face-Recognition";
+    const MODEL_ID = "face-detection";
+    const MODEL_VERSION_ID = "9491c36f920544d686c7bbcdd4b0557f";
+    const IMAGE_URL = "https://samples.clarifai.com/metro-north.jpg";
+
+    const raw = JSON.stringify({
+      user_app_id: {
+        user_id: USER_ID,
+        app_id: APP_ID,
+      },
+      inputs: [
+        {
+          data: {
+            image: {
+              url: IMAGE_URL,
+            },
+          },
+        },
+      ],
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        Authorization: "Key " + PAT,
+      },
+      body: raw,
+    };
+
+    fetch(
+      `https://api.clarifai.com/v2/models/${MODEL_ID}/outputs`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((response) => console.log(response));
+  };
+
   render() {
     return (
       <div className="App">
         <Navigation />
         <Logo />
-        <ImageLinkForm />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
         {/* <FaceRecognition /> */}
       </div>
     );
--- src/Components/ImageLinkForm/ImageLinkForm.jsx
+++ src/Components/ImageLinkForm/ImageLinkForm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./ImageLinkForm.css";
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div className="text-center  relative bottom-32  grid justify-center items-center h-screen z-0">
       <div className="grid justify-center items-center shadow shadow-cyan-100 bgimg rounded-xl">
@@ -12,10 +12,14 @@ const ImageLinkForm = () => {
           <div className="p-4 w-700">
             <input
               placeholder="Paste your image URL here"
-              className="text-lg w-3/4 mr-auto ml-auto rounded-xl bimg border-2 p-1"
+              className="text-lg w-3/4 mr-auto ml-auto bimg border-2 p-1 "
               type="text"
+              onChange={onInputChange}
             />
-            <button className="w-1/3 hover:scale-105 duration-300  py-2 px-3 inline-block  m-4 bbg text-2xl">
+            <button
+              className="w-1/3 hover:scale-105 duration-300  py-2 px-3 inline-block  m-4 bbg text-2xl"
+              onClick={onButtonSubmit}
+            >
               Detect
             </button>
           </div>
